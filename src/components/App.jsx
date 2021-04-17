fc4ee1bba51229d6b1ebeefe273923d3693e82c2
irjar <53143879+irjar@users.noreply.github.com>
2021-04-17 21:20:33 +0100

    change the map function into an arrow function in index.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,19 +2,10 @@ import React from "react";
 import Card from "./Card";
 import emojipedia from "../emojipedia"
 
+
+
 // take a single emoji term; return a single Card component 
 // and pass the props: key, emojiIcon, emojiName and emojiMeaning
-function createCard(newCard){
-  return(
-    <Card
-    key={newCard.id}
-    emojiIcon={newCard.emoji}
-    emojiName={newCard.name}
-    emojiMeaning={newCard.meaning}
-    />
-  );
-}
-
 // map through the emojipedia array and render Card components
 function App() {
   return (
@@ -23,7 +14,14 @@ function App() {
         <span>emojipedia</span>
       </h1>
       <dl className="dictionary">
-        {emojipedia.map(createCard)}
+        {emojipedia.map( newCard =>(
+    <Card
+    key={newCard.id}
+    emojiIcon={newCard.emoji}
+    emojiName={newCard.name}
+    emojiMeaning={newCard.meaning}
+    />
+        ))}
     </dl>
     </div>
   );
